601d9bebec91b320ea1ba630645dad379894e41d
Gomez-2003 <89650548+Gomez-2003@users.noreply.github.com>
2021-09-07 10:52:55 -0500

    Create GØMĘZ-bot.js

---
+++ GØMĘZ-bot.js
@@ -0,0 +1,33 @@
+const { WAConnection, MessageType, Mimetype, } = require('@adiwajshing/baileys');
+
+const fs = require('fs');
+
+const prefix = '.'
+
+async function iniciar () { 
+
+        const client = new WAConnection()
+
+        client.logger.level = 'warn'
+
+        client.on('qr', () => {
+
+        })
+
+        fs.existsSync('./Gomez-2003.json') && client.loadAuthInfo('.Gomez-2003.json')
+
+        client.on('connecting', () => {
+
+        console.log('Conectando')
+
+        })
+
+        client.on('open', () => {
+
+        console.log('Listo se contecto correctamente comienza a usar el bot')
+
+        })
+
+        await client.connect({timeoutMs: 30*1000})
+
+        fs.writeFileSync('./Gomez-2003', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
